d9d397b0db0559f6c6139f0df1bdd5628afdc86b
Arley Padua <arley_padua>
2022-09-01 20:58:50 +0200

    adds an editable area to the current praise

--- src/components/CurrentPraise/CurrentPraise.js
+++ src/components/CurrentPraise/CurrentPraise.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { usePraiseQueueContext } from '../PraiseQueue/PraiseQueueContext'
 import './CurrentPraise.css'
 
 export default function CurrentPraise() {
-  const { praiseQueue: { current: { praise: { title, content } } } } = usePraiseQueueContext()
-  console.log(title, content)
+  const [currentPraise, setCurrentPraise] = useState()
+  const { praiseQueue } = usePraiseQueueContext()
+  const { title, content } = praiseQueue?.current?.praise ?? {}
 
-  return (
+  useEffect(() => {
+    if (title && content) {
+      setCurrentPraise({
+        title,
+        content
+      })
+    }
+  }, [title, content])
+
+  const handleContentChange = event => {
+    setCurrentPraise((prev) => ({ ...prev, content: event.target.value }));
+  };
+
+  return currentPraise && (
     <>
-      <section className='current_praise__page'>
-        <h1>{title}</h1>
-        <p>P teste 1</p>
-        <p>P teste 2</p>
-        <p>P teste 3</p>
-      </section>
+      <textarea 
+        value={currentPraise.content}
+        onChange={handleContentChange}
+      ></textarea>
     </>
   )
 }
