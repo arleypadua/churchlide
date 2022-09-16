62693a9c2913aba5a4629ade0ce67fa23e81437d
Arley Padua <arley_padua>
2022-09-16 18:45:19 +0200

    feat: adds pwa support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./auth/authConfig";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './style.css'
 
 const msalInstance = new PublicClientApplication(msalConfig);
@@ -30,6 +31,11 @@ root.render(
     </MsalProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
